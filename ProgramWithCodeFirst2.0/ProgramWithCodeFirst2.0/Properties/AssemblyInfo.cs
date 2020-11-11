e9ff77c35f9db2d6a9d6b2e2213a035b96e4a79c
Nawiarra <nika.berezovskaya@ukr.net>
2020-11-11 18:25:41 +0200

    Initial multilayer project

---
+++ ProgramWithCodeFirst2.0/ProgramWithCodeFirst2.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ProgramWithCodeFirst2.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProgramWithCodeFirst2.0")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0f7241d7-b054-46dc-a3c9-7ffcae6921c3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProgramWithCodeFirst2.0/WoodWorkshop.Data/WoodWorkshopContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WoodWorkshop.Data.Models;
+
+namespace WoodWorkshop.Data
+{
+    public class WoodWorkshopContext: DbContext
+    {
+        public WoodWorkshopContext() : base("Data Source =(LocalDB)\\MSSQLLocalDB;Initial Catalog = MultiLayerExampleDB;Integrated Security=true")
+        {
+
+        }
+        public WoodWorkshopContext(string connectionString): base(connectionString)
+        {
+
+        }
+
+        public DbSet <WoodFurnitureOrder> WoodFurnitureOrders { get; set; }
+        public DbSet <FurnitureType> FurnitureTypes { get; set; }
+        public DbSet<WoodType> WoodTypes { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Epoxy> Epoxys { get; set; }
+    }
+}
